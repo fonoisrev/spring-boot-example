89626801116d83e00e780d16784fb293309defad
fonoisrev <wuhangdeyouxiang@126.com>
2018-08-17 15:03:08 +0800

    更改configure模块名称为小写

--- configuration/src/main/java/com/github/fonoisrev/configuration/MyConfiguration.java
+++ configuration/src/main/java/com/github/fonoisrev/configuration/MyConfiguration.java
@@ -2,22 +2,31 @@ package com.github.fonoisrev.configuration;
 
 import com.github.fonoisrev.bean.Apple;
 import com.github.fonoisrev.bean.Banana;
+import org.springframework.asm.SpringAsmInfo;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
     
-    @Bean
+    @Bean("apple")
     public Apple apple() {
         return new Apple();
     }
     
     @Bean
     public Banana banana() {
-        return new Banana(apple());// is the two apple the same?
+        return new Banana(apple2());// is the two apple the same?
+    }
+    
+    @Bean("apple")
+    public Apple apple2() {
+        return new Apple();
     }
     
     @Bean
@@ -29,9 +38,13 @@ public class MyConfiguration {
             @Autowired
             Banana banana;
         
+            @Autowired
+            BeanFactory factory;
+            
             @Override
             public void run(String... args) throws Exception {
                 boolean isSame = apple == banana.getApple();
+                
                 System.out.println("Apple == Banana.apple ? " + isSame);
             }
         };
